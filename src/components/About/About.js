077c4d4439761c58e9e1a25de60e4f024f9856db
Your Name <mushfiqur15-10872@diu.edu.bd>
2022-10-04 16:16:10 +0600

    Route added

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const About = () => {
+    return (
+        <div>
+            <h5>This is the ture Real About me page</h5>
+        </div>
+    );
+};
+
+export default About;
